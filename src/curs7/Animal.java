a25a23f5992ad5581d52234ae5101557abdb6fb3
eliza <mihaieliza@gmail.com>
2023-03-13 19:48:39 +0200

    override examples

---
+++ src/curs7/Animal.java
@@ -0,0 +1,12 @@
+package curs7;
+
+public class Animal {
+
+    public void makeSound() {
+        System.out.println("Make a generic sound!");
+    }
+
+    public void metodaAnimal() {
+        System.out.println("Metoda e disponibila numai in clasa Animal.");
+    }
+}
---
+++ src/curs7/Deer.java
@@ -0,0 +1,13 @@
+package curs7;
+
+public class Deer extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meee!");
+    }
+
+    public void eatGrass() {
+        System.out.println("I like eating grass!");
+    }
+}
